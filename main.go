0104a8a9232fcfd3ba6e9929482c7652ee7f7fb3
rezafauzan <rezafauzan593@gmail.com>
2026-02-16 13:42:06 +0700

    Feat: add Antrian struct

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Antrian struct {
+	Nama  string
+	Waktu int
+}
+
+var antrian = []Antrian{
+	{Nama: "Aetherion", Waktu: 4},
+	{Nama: "Zythera", Waktu: 6},
+	{Nama: "Vaelthryn", Waktu: 1},
+	{Nama: "Eryndor", Waktu: 9},
+}
+
+func main() {	}
