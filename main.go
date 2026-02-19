e34a96c42581c0b12f936533c63457b3dc1bf981
rezafauzan <rezafauzan593@gmail.com>
2026-02-19 16:44:23 +0700

    Feat: update better printed output

--- main.go
+++ main.go
@@ -12,24 +12,26 @@ type Antrian struct {
 }
 
 var antrian = []Antrian{
-	{Nama: "Aetherion", Waktu: 4},
-	{Nama: "Zythera", Waktu: 6},
-	{Nama: "Vaelthryn", Waktu: 1},
-	{Nama: "Eryndor", Waktu: 9},
+	{Nama: "Aetherion", Waktu: 1},
+	{Nama: "Zythera", Waktu: 2},
+	{Nama: "Vaelthryn", Waktu: 4},
+	{Nama: "Eryndor", Waktu: 4},
 }
 
-func PrintAntrian(wg *sync.WaitGroup) {
+func PrintAntrian(data chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("Memulai antrian")
 	for x := range antrian {
 		time.Sleep(time.Duration(antrian[x].Waktu) * time.Second)
-		fmt.Printf("Pesanan %s selesai \n" ,antrian[x].Nama)
+		fmt.Printf("Pesanan %s selesai (%d) \n" ,antrian[x].Nama, antrian[x].Waktu)
 	}
 }
 
 func main() {
+	data := make(chan string)
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go PrintAntrian(&wg)
+	go PrintAntrian(data, &wg)
 	wg.Wait()
+	fmt.Println("Antrian Selesai")
 }
